4e5f2d3cd2493946405f4068a3861fc858dd3bff
AtulPrakash <atulprakash2003@gmail.com>
2024-02-16 16:12:56 +0530

    TWO POINTER APPROACH

---
+++ LECTURE37.java
@@ -0,0 +1,29 @@
+class TwoPointerConcept{
+    public static void main(String [] args)
+    {
+        int arr[] = {10,20,35,50,75,80};
+        int x = 95;
+        int l = 0;
+        int r = arr.length - 1;
+        boolean found = false;// flag
+        while(l<r)
+        {
+            int sum = arr[l] + arr[r];
+            if(sum == x)
+            {
+                System.out.println("Found "+ x +" as sum of values on index: "+ l +" and index: "+ r);
+                found = true;
+                break;
+            }
+            else if(sum<x)
+            {
+                l++;
+            }
+            else //sum>x
+            {
+                r--;
+            }
+        }
+        if(!found)System.out.println("Sum no found.");
+    }
+}
